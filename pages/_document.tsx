fb5a24cfaa1b39039a7f5cb57c04faa4faad61f2
cubenoy22 <cubenoy22@gmail.com>
2020-09-02 18:04:37 +0900

    Use Noto Sans JP

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,14 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head />
-        <body>
+      <Html lang="ja">
+        <Head>
+          <link 
+            href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:100"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="font-body font-hairline">
           <Main />
           <NextScript />
         </body>
