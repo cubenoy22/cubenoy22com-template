43de5a7fefdcc9ed92be2d173d888bfa63ad5c15
cubenoy22 <cubenoy22@gmail.com>
2020-09-03 12:02:41 +0900

    Improve design

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,19 +8,19 @@ const Footer = () => {
           <div className="flex flex-row lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="/"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white py-3 duration-200 transition-colors mb-6 lg:mb-0"
             >
               GitHub
             </a>
             <a
               href="/"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white py-3 duration-200 transition-colors mb-6 lg:mb-0"
             >
               SoundCloud
             </a>
             <a
               href="/"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white py-3 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Twitter
             </a>
