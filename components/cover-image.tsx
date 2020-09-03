8ed316857fa3bdf7db228560337bb22a056cbef5
cubenoy22 <cubenoy22@gmail.com>
2020-09-03 12:00:59 +0900

    Use responsive jpeg images

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -8,14 +8,19 @@ type Props = {
 }
 
 const CoverImage = ({ title, src, slug }: Props) => {
+  const responsiveImage = require(`../public${src}?resize&sizes[]=375&sizes[]=768&sizes[]=1000`)
   const image = (
-    <img
-      src={src}
-      alt={`Cover Image for ${title}`}
-      className={cn('shadow-small', {
-        'hover:shadow-medium transition-shadow duration-200': slug,
-      })}
-    />
+    <picture>
+      <img
+        src={responsiveImage.src}
+        srcSet={responsiveImage.srcSet}
+        alt={`Cover Image for ${title}`}
+        className={cn('shadow-small', {
+          'hover:shadow-medium transition-shadow duration-200': slug,
+        })}
+        loading='lazy'
+      />
+    </picture>
   )
   return (
     <div className="sm:mx-0">
