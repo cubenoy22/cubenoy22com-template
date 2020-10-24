912cf1273f1c2608ff9c887ea2441239a1be9f63
cubenoy22 <cubenoy22@gmail.com>
2020-10-24 17:53:27 +0900

    Update hero-post layout

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -11,11 +11,11 @@ const HeroPost = ({
   post: { title, coverImage, slug, date, excerpt, tags }
 }: Props) => {
   return (
-    <section>
-      <div className="mb-8">
+    <section className="mb-8 md:flex md:items-center">
+      <div className="mb-8 md:mb-0 md:mr-8 md:w-1/2">
         <CoverImage title={title} src={coverImage} slug={slug} />
       </div>
-      <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
+      <div className="md:grid md:w-1/2 md:row-gap-8 lg:row-gap-16 lg:row-gap-8 mb-20 md:mb-0">
         <div>
           <h3 className="mb-4 text-3xl lg:text-6xl leading-tight">
             <Link as={`/posts/${slug}`} href="/posts/[slug]">
